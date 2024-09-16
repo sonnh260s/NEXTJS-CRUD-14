6d79acf39cd331624d70cfaa4da26a4331254c4a
sonnh260s <sonnh260@gmail.com>
2024-09-16 16:33:40 +0700

    Update UI pagination component

--- src/app/employee/dashboard/pagination.js
+++ src/app/employee/dashboard/pagination.js
@@ -1,28 +1,51 @@
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
-  
-    return (
-      <div className="flex justify-center mt-4">
-        <nav aria-label="Pagination">
-          <ul className="flex list-style-none">
-            {pages.map((page) => (
-              <li key={page} className="mx-1">
-                <button
-                  onClick={() => onPageChange(page)}
-                  className={`px-3 py-1 border border-gray-300 rounded-md text-sm font-medium ${
-                    page === currentPage ? 'bg-gray-200' : 'bg-white'
-                  }`}
-                >
-                  {page}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    );
-  };
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <div className="flex justify-center mt-4">
+      <nav aria-label="Pagination">
+        <ul className="flex list-style-none">
+          {/* Nút Previous */}
+          <li className="mx-1">
+            <button
+              onClick={() => onPageChange(currentPage - 1)}
+              disabled={currentPage === 1} // Vô hiệu hóa nếu là trang đầu tiên
+              className="px-3 py-1 border border-gray-300 rounded-md text-sm font-medium bg-white"
+            >
+              Previous
+            </button>
+          </li>
+
+          {pages.map((page) => (
+            <li key={page} className="mx-1">
+              <button
+                onClick={() => onPageChange(page)}
+                className={`px-3 py-1 border border-gray-300 rounded-md text-sm font-medium ${
+                  page === currentPage ? 'bg-gray-200' : 'bg-white'
+                }`}
+              >
+                {page}
+              </button>
+            </li>
+          ))}
+
+          {/* Nút Next */}
+          <li className="mx-1">
+            <button
+              onClick={() => onPageChange(currentPage + 1)}
+              disabled={currentPage === totalPages} // Vô hiệu hóa nếu là trang cuối cùng
+              className="px-3 py-1 border border-gray-300 rounded-md text-sm font-medium bg-white"
+            >
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
   
   export default Pagination;
   
